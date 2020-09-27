e3094d0c3bbfde690a5f05fec7f678be0cf62689
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2020-09-27 12:09:58 +0900

    refactored code
    changed format into Google rules
    splited code in main.c into max7219.h and vatista_common.h

---
+++ vatista_common.h
@@ -0,0 +1,32 @@
+/*
+ * vatista_common.h
+ *
+ * Created: 2020/09/23 22:49:46
+ *  Author: lum1narie
+ */
+
+
+#ifndef VATISTA_COMMON_H_
+#define VATISTA_COMMON_H_
+
+#include <avr/io.h>
+
+#define ENABLE_POWER_SAVE
+
+#define CLK_PORT PORTC
+#define DOUT_PORT PORTC
+#define CS_PORT PORTC
+
+#define CLK_PIN PORTC0
+#define DOUT_PIN PORTC2
+#define CS_PIN PORTC1
+
+#define TCH_CLOCK 5
+
+#define get_bit(n, bit) (((n) >> (bit)) & 1)
+#define set_bit(n, bit) ((n) |= (uint8_t)(_BV(bit)))
+#define clr_bit(n, bit) ((n) &= ~(uint8_t)(_BV(bit)))
+
+#define F_CPU 1048576UL
+
+#endif /* VATISTA_COMMON_H_ */
